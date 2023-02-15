07b1ee26d2e8205a6b02358a79b61be566a02d53
edwinyjchen <edwinyjchen@tencent.com>
2023-02-14 17:29:29 -0800

    update scripts

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in proc
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: '/tjs-journey-preview/',
+    base: '/tjs-journey/',
     server:
     {
         host: true,
